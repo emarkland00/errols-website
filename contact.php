f25e9f123cf14a32173e90433f5761d52c4caf67
Errol Markland <ja23jr@gmail.com>
2013-07-27 23:01:55 -0400

    Lots of improvement on the contact form.

--- contact.php
+++ contact.php
@@ -16,57 +16,60 @@
 			
 			<div id="navigation">
 				<ul>
-					<li class='nav-item'><a href="/index.php">Home</a></li>
+					<li class='nav-item'><a href="/">Home</a></li>
 					<li class='nav-item'><a href="/resume.php" title="About me!">Resume</a></li>
 					<li class='nav-item'><a href="/projects.php" title="Check out some of my projects!">Projects</a></li>
 					<li class='nav-item' id="active-nav-item" title="Currently on this page"><a href="/contact.php">Contact</a></li>
 				</ul>
 			</div>
-			<div class="separator"></div>
 			
 			<div class="content">
-				<p>Glad you would like to keep in touch.</p>
-				
-				
 				<div id="contact-form">
+					<p>Need something? Send me a message!</p>
+					
 					<div class="form-item">
-						<input type="text" placeholder="Subject" />
+						<input type="text" placeholder="Message Subject" id="subject" />
 					</div>
 					
 					<div class="form-item">
-						<input type="text" placeholder="Details" />
+						<textarea  placeholder="Details on subject matter" id="details"></textarea>
 					</div>
 					
 					<div class="form-item">
-						<input type="button" value="Send" />
+						<input type="submit" value="Send" id="submit-button" />
 					</div>
 				</div>
-
-				<p>Form is unavailable at the moment, but will be available very soon!</p>
 				
-				<p>For now, email me at <a href="mailto:errol.markland@gmail.com">this address</a> in the meantime. Please be specific in your inquiry</p>
-				
-				 <p>
-				 	I'm also on 
-				 	<a href="https://www.facebook.com/emark00" target="_blank">facebook</a>, 
-				 	<a href="https://plus.google.com/100925522890315697168/about/p/pub" target="_blank">Google+</a>, 
-				 	<a href="http://www.linkedin.com/pub/errol-markland/31/674/809" target="_blank">LinkedIn</a>, and
-				 	<a href="https://twitter.com/mr_markland">Twitter</a>.
-				 </p>
-				 
-				 <!-- 
-					Just fill out the form below and I'll get back to you as soon as possible.
-				 -->
+				<div id="social-links">
+					<p>I'm available on these networks!</p>
+					<div id="social-icon-container">
+						<a href="https://www.facebook.com/emark00" target="_blank">
+							<div class="social-icon">
+								<br /><img src="/images/fb_logo.png" alt="Facebook" />
+						 	</div>
+					 	</a>
+					 	
+					 	<a href="https://plus.google.com/100925522890315697168/about/p/pub" target="_blank">
+						 	<div class="social-icon">
+						 		<br /><img src="/images/gplus_logo1.png" alt="Google+" />
+						 	</div>
+					 	</a>
+					 	
+					 	<a href="http://www.linkedin.com/pub/errol-markland/31/674/809" target="_blank">
+						 	<div class="social-icon"><br />
+						 		<img src="/images/linkedin_logo1.png" alt="LinkedIn" />
+						 	</div>
+					 	</a>
+					 	
+					 	<a href="https://twitter.com/mr_markland" target="_blank">
+						 	<div class="social-icon"><br />
+						 		<img src="/images/twitter_logo1.png" alt="Twitter" />
+						 	</div>
+					 	</a>
+				 	</div>
+				 </div>
+				 <div class="clearer"></div>
 			</div>
 		</div>
-		
-		<div id="footer">
-			<a href="/index.php">Home</a> | 
-			<a href="/resume.php">Resume</a> | 
-			<a href="/projects.php">Projects</a> | 
-			<a href="/contact.php">Contact</a>
-		</div>
-		
-		<div id="copyright">&copy; Errol Markland, 2013.</div>
 	</body>	
 </html>
