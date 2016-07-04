7661e028973b9d862c50e5e2deebdfc22f9caa1f
Errol Markland <errol.markland@gmail.com>
2016-07-03 20:54:42 -0400

    Finally updating the layout and my script to work with the new stuff.

--- handlers/latest.php
+++ handlers/latest.php
@@ -3,24 +3,41 @@ require_once("Article.php");
 
 function is_valid_request() {
     // token existence
-    if (!isset($_SESSION['token'])) return false;
-    $tkn = $_SESSION['token'];
+    if (!isset($_SESSION['token'])) {
+        echo "Missing token on server";
+        return false;
+    }
+    $token = $_SESSION['token'];
 
-    // token expiration (5 min)
-    if (!isset($_SESSION['token_time'])) return false;
+    if (!isset($_SESSION['token_time'])) {
+        echo "Missing token to check expiration";
+        return false;
+    }
     $time = $_SESSION['token_time'];
-    if (time() - $time > 300) return false;
+    if (time() - $time > 30) {
+        echo "Missing token (it's expired)";
+        return false;
+    }
 
     // fetched token
-    if (!isset($_GET['token'])) return false;
-    $currentTkn = $_GET['token'];
+    if (!isset($_COOKIE['sc'])) {
+        return false;
+    }
 
-    return $currentTkn === $tkn;
+    $cookieToken = $_COOKIE['sc'];
+    $computedVal = md5($token ^ $time);
+
+    clear_session();
+    return $cookieToken === $computedVal;
 }
 
 function clear_session() {
     unset($_SESSION['token']);
     unset($_SESSION['token_time']);
+
+    // remove cookie
+    unset($_COOKIE['sc']);
+    setcookie('sc', null, -1, "/", true, true);
 }
 
 function get_results($count) {
@@ -38,11 +55,20 @@ function get_results($count) {
 
 if (is_valid_request()) {
     header('Content-type: application/json');
-    echo get_results($_GET['count']);
+    $count = 0;
+
+    if (isset($_POST['count'])) {
+        $count = (int)$_POST['count'];
+
+        if ($count > 10) {
+            $count = 10;
+        } else if ($count < 0) {
+            $count = 0;
+        }
+    }
+
+    echo get_results($count);
 } else {
     header('HTTP/1.1 400 Bad Request', true, 400);
 }
-
-clear_session();
-
 ?>
