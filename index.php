7d4f3e96417fbc0ad7fe74a94266351811389ac8
Errol Markland <ja23jr@gmail.com>
2016-12-26 01:08:05 -0500

    Updated handlebars

--- index.php
+++ index.php
@@ -36,175 +36,176 @@
     </head>
     <body>
 
-    <nav id="nav">
-        <ul class="container">
-            <li><a href="#top">Top</a></li>
-            <li><a href="#articles">Articles</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-    </nav>
+        <nav id="nav">
+            <ul class="container">
+                <li><a href="#top">Top</a></li>
+                <li><a href="#articles">Articles</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
 
-    <!-- Home -->
-    <div class="wrapper style1 first">
-        <article class="container" id="top">
-            <div class="row">
-                <div class="4u 12u(mobile)">
-                    <span class="image fit">
-                        <img src="/images/me.jpg" alt="My picture" />
-                    </span>
-                </div>
-                <div class="8u 12u(mobile)">
-                    <header>
-                        <h1>Hi. I&#39;m
-                        <strong>Errol G. Markland Jr.</strong>.</h1>
-                    </header>
-                    <p>A developer who likes to explore the various venues of technology.</p>
-                    <a href="#contact" class="button big scrolly">Get in touch</a>
-                </div>
-            </div>
-        </article>
-    </div>
-
-    <!-- Articles -->
-    <div class="wrapper style2">
-        <article id="articles">
-            <header>
-                <h2>Here are some articles I've read lately.</h2>
-            </header>
-            <div class="container" id="latest-articles">
-                <div class="row" id="latest-entry-content"></div>
-            </div>
-            <footer>
-                Powered by the <a href="https://getpocket.com/" target="_blank">Pocket API</a>.
-            </footer>
-        </article>
-    </div>
-
-    <!-- Projects -->
-    <div class="wrapper style3">
-        <article id="projects">
-            <header>
-              <h2>Projects</h2>
-              <p>
-      					Some of the projects I've worked on can be found at my
-      					<a href="https://github.com/emarkland" target="_blank">Github</a>.
-      				</p>
-
-      				<p>In the future, I'll make life a bit easier by actually listing the projects I've done here.</p>
-            </header>
-        </article>
-    </div>
-
-    <!-- Contact -->
-    <div class="wrapper style4">
-        <article id="contact" class="container 75%">
-            <header>
-                <h2>Let's chat!</h2>
-                <p>Feel free to reach out to me. Whether if its to network, to do some business, or even just to say hey.</p>
-            </header>
-            <div>
+        <!-- Home -->
+        <div class="wrapper style1 first">
+            <article class="container" id="top">
                 <div class="row">
-                    <div class="12u">
-                        <form id='contact-form'>
-                            <div>
-                                <div class="row">
-                                    <div class="6u 12u(mobile)">
-                                        <input type="text" name="name" id="name" placeholder="Name" />
-                                    </div>
-                                    <div class="6u 12u(mobile)">
-                                        <input type="text" name="email" id="email" placeholder="Email" />
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="12u">
-                                        <input type="text" name="subject" id="subject" placeholder="Subject" />
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="12u">
-                                        <textarea name="message" id="message" placeholder="Message"></textarea>
-                                    </div>
-                                </div>
-                                <div class="row 200%">
-                                    <div class="12u">
-                                        <ul class="actions">
-                                            <li>
-                                                <button type="submit" id='submit-button'>Send Message</button>
-                                            </li>
-                                            <li>
-                                                <button type="reset" class="alt">Clear Form</button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                    <div class="4u 12u(mobile)">
+                        <span class="image fit">
+                            <img src="/images/me.jpg" alt="My picture" />
+                        </span>
+                    </div>
+                    <div class="8u 12u(mobile)">
+                        <header>
+                            <h1>Hi. I&#39;m
+                            <strong>Errol G. Markland Jr.</strong>.</h1>
+                        </header>
+                        <p>A developer who likes to explore the various venues of technology.</p>
+                        <a href="#contact" class="button big scrolly">Get in touch</a>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="12u">
-                        <hr />
-                        <h3>Find me on ...</h3>
-                        <ul class="social">
-                            <li>
-                                <a href="https://www.facebook.com/emark00" target="_blank" class="icon fa-facebook">
-                                    <span class="label">Facebook</span>
-                                </a>
-                            </li>
+            </article>
+        </div>
 
-                            <li>
-                                <a href="https://twitter.com/mr_markland" target="_blank" class="icon fa-twitter">
-                                    <span class="label">Twitter</span>
-                                </a>
-                            </li>
+        <!-- Articles -->
+        <div class="wrapper style2">
+            <article id="articles">
+                <header>
+                    <h2>Here are some articles I've read lately.</h2>
+                </header>
+                <div class="container" id="latest-articles">
+                    <div class="row" id="latest-entry-content"></div>
+                </div>
+                <footer>
+                    Powered by the <a href="https://getpocket.com/" target="_blank">Pocket API</a>.
+                </footer>
+            </article>
+        </div>
 
-                            <li>
-                                <a href="https://www.instagram.com/mr_markland/" target="_blank" class="icon fa-instagram">
-                                    <span class="label">Instagram</span>
-                                </a>
-                            </li>
+        <!-- Projects -->
+        <div class="wrapper style3">
+            <article id="projects">
+                <header>
+                  <h2>Projects</h2>
+                  <p>
+          					Some of the projects I've worked on can be found at my
+          					<a href="https://github.com/emarkland" target="_blank">Github</a>.
+          				</p>
 
-                            <li>
-                                <a href="https://www.linkedin.com/in/errol-markland-80967431" target="_blank" class="icon fa-linkedin">
-                                    <span class="label">LinkedIn</span>
-                                </a>
-                            </li>
+          				<p>In the future, I'll make life a bit easier by actually listing the projects I've done here.</p>
+                </header>
+            </article>
+        </div>
 
-                            <li>
-                                <a href="https://github.com/emarkland" target="_blank" class="icon fa-github">
-                                    <span class="label">Github</span>
-                                </a>
-                            </li>
-                        </ul>
-                        <hr />
+        <!-- Contact -->
+        <div class="wrapper style4">
+            <article id="contact" class="container 75%">
+                <header>
+                    <h2>Let's chat!</h2>
+                    <p>Feel free to reach out to me. Whether if its to network, to do some business, or even just to say hey.</p>
+                </header>
+                <div>
+                    <div class="row">
+                        <div class="12u">
+                            <form id='contact-form'>
+                                <div>
+                                    <div class="row">
+                                        <div class="6u 12u(mobile)">
+                                            <input type="text" name="name" id="name" placeholder="Name" />
+                                        </div>
+                                        <div class="6u 12u(mobile)">
+                                            <input type="text" name="email" id="email" placeholder="Email" />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="12u">
+                                            <input type="text" name="subject" id="subject" placeholder="Subject" />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="12u">
+                                            <textarea name="message" id="message" placeholder="Message"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="row 200%">
+                                        <div class="12u">
+                                            <ul class="actions">
+                                                <li>
+                                                    <button type="submit" id='submit-button'>Send Message</button>
+                                                </li>
+                                                <li>
+                                                    <button type="reset" class="alt">Clear Form</button>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="12u">
+                            <hr />
+                            <h3>Find me on ...</h3>
+                            <ul class="social">
+                                <li>
+                                    <a href="https://www.facebook.com/emark00" target="_blank" class="icon fa-facebook">
+                                        <span class="label">Facebook</span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="https://twitter.com/mr_markland" target="_blank" class="icon fa-twitter">
+                                        <span class="label">Twitter</span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="https://www.instagram.com/mr_markland/" target="_blank" class="icon fa-instagram">
+                                        <span class="label">Instagram</span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="https://www.linkedin.com/in/errol-markland-80967431" target="_blank" class="icon fa-linkedin">
+                                        <span class="label">LinkedIn</span>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="https://github.com/emarkland" target="_blank" class="icon fa-github">
+                                        <span class="label">Github</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <hr />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <footer>
-                <ul id="copyright">
-                    <!-- Show fun fact based on today's date. Use number's api? -->
-                </ul>
-            </footer>
-        </article>
-    </div>
+                <footer>
+                    <ul id="copyright">
+                        <!-- Show fun fact based on today's date. Use number's api? -->
+                    </ul>
+                </footer>
+            </article>
+        </div>
 
 
-    <!-- Scripts -->
-    <script src="/js/libs/jquery-2.1.1.min.js"></script>
-    <script src="/js/jquery.scrolly.min.js"></script>
-    <script src="/js/libs/handlebars-v1.2.0.js"></script>
-    <script src="/js/libs/moment.min.js"></script>
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/jquery.scrolly.min.js"></script>
-    <script src="/js/skel.min.js"></script>
-    <script src="/js/skel-viewport.min.js"></script>
-    <script src="/js/util.js"></script>
-    <!--[if lte IE 8]><script src="/js/ie/respond.min.js"></script><![endif]-->
+        <!-- Scripts -->
+        <script src="/js/libs/jquery-2.1.1.min.js"></script>
+        <script src="/js/jquery.scrolly.min.js"></script>
+        <script src="/js/libs/handlebars-v4.0.5.js"></script>
+        <script src="/js/libs/moment.min.js"></script>
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/jquery.scrolly.min.js"></script>
+        <script src="/js/skel.min.js"></script>
+        <script src="/js/skel-viewport.min.js"></script>
+        <script src="/js/util.js"></script>
+        <!--[if lte IE 8]><script src="/js/ie/respond.min.js"></script><![endif]-->
 
-    <script src="/js/main.js"></script></body>
-
-    <script src="/js/latest-entry.js"></script>
-    <script src="/js/contact.js"></script>
-    <script src="/js/ga.js"></script>
+        <script src="/js/main.js"></script>
+        <script src="/js/article-template.js"></script>
+        <script src="/js/latest-entry.js"></script>
+        <script src="/js/contact.js"></script>
+        <script src="/js/ga.js"></script>
+    </body>
 </html>
