9b1ec53a546851e65b38a786f1eccb9b30908661
Errol Markland <ja23jr@gmail.com>
2017-04-14 23:43:16 -0400

    Store stuff in memcached for a minute

--- handlers/latest.php
+++ handlers/latest.php
@@ -43,9 +43,34 @@ function clear_session() {
     setcookie('sc', null, -1, "/", true, true);
 }
 
+function get_memcached() {
+    if (!class_exist('Memcached')) {
+        return null;
+    }
+    $mc = new Memcached();
+    $mc->addServer("127.0.0.1", 11211);
+    return $mc;
+}
+
 function get_results($count) {
-    $client = new PocketClient();
-    $arr = $client->getArticles();
+    $arr = null;
+    $key = 'pocket:items';
+
+    // pull items from cache
+    $mc = get_memcached();
+    if ($mc != null) {
+        $arr = $mc->get($key);
+    }
+
+    if ($arr == null) {
+        $client = new PocketClient();
+        $arr = $client->getArticles();
+
+        // store items in cache for a minute
+        if ($mc != null) {
+            $mc->set($key, $arr, time() + 60);
+        }
+    }
     $res = array();
     foreach ($arr as $a) {
         $host = parse_url($a->getGivenURL(), PHP_URL_HOST);
